860355aea7466d1270a8f6c2a237db59e7acd4ae
tutich <pkoinange02@gmail.com>
2023-11-28 01:08:50 +0300

    slider dsign

--- src/App.js
+++ src/App.js
@@ -5,37 +5,42 @@ import circlePat from '../src/images/pattern-circles.svg';
 function App() {
   return (
     <div className="app">
-      <div>
-      <img src='circlePat' alt='circle'/>
-      </div>
-     <div className="header">
-       <h2>Simple, traffic-based pricing</h2>
-        <p>Sign-up for our 30 day trial. No credit card required.</p>
-      </div>
-      <div className="container">
-        <div class="slide">
-          <p>100K PAGEVIEWS</p>
-          <span>$16.00<p>/ month</p></span>
-          <input type="range"/>
+     <div className="wrapper">
+       <div className="header">
+         <h2>Simple, traffic-based pricing</h2>
+          <p>Sign-up for our 30 day trial. No credit card required.</p>
         </div>
-        <div className="pricing">
-          <p>Monthly Billing</p>
-          <label class="switch">
-            <input type="checkbox"/>
-            <span class="slider round"></span>
-          </label>
-          <p>Yearly Billing  <span>25% discount</span></p>
-          <hr/>
-          <p>Unlimited Websites</p>
-          <p>100% data ownership</p>
-          <p>Email reports</p>
-          <div className='but'>
-            <button className="btn">Start my trial</button>
+        <div className="container">
+          <div class="slide">
+            <p>100K PAGEVIEWS</p>
+            <input className='slider' type="range"/>
+            <img
+              src="./images/icon-slider.svg"
+              alt=""
+              class="slider-icon"
+              draggable="true"/> 
+              
+            <span>$16.00<p>/ month</p></span>
           </div>
-          
+          <div className="pricing">
+            <p>Monthly Billing</p>
+            <label class="switch">
+              <input type="checkbox"/>
+              <span class="slider round"></span>
+            </label>
+            <p>Yearly Billing  <span>25% discount</span></p>
+            <hr/>
+            <p>Unlimited Websites</p>
+            <p>100% data ownership</p>
+            <p>Email reports</p>
+            <div className='but'>
+              <button className="btn">Start my trial</button>
+            </div>
+       
+          </div>
         </div>
-      </div>
-      
+       
+     </div>
     </div>
   );
 }
